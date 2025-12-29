8fda51b65de54aac91b74c92056ed052315b5f74
Jakub Oleksiak <yakubofficialmail@gmail.com>
2025-12-30 00:36:29 +0100

    jakub: renderer scaffolding

--- ft_vox.hpp
+++ ft_vox.hpp
@@ -19,6 +19,11 @@
 namespace ft {
 
     class Window;
+    class Buffer;
+    class VertexBuffer;
+    class IndexBuffer;
+    class Shader;
+    class Texture;
     class Renderer;
 
     class Window {
@@ -50,7 +55,121 @@ namespace ft {
             bool ready(void);
             bool shouldQuit(void);
     };
+    
+    class Buffer {
+        private:
+            GLuint m_id;
 
+        public:
+            Buffer(void);
+            Buffer(const Buffer &);
+            ~Buffer(void);
+
+            const Buffer &operator = (const Buffer &);
+
+            Buffer &bind(void) = 0;
+            BUffer &unbind(void) = 0;
+
+            GLuint getID(void);
+    };
+    
+    class VertexBuffer : public Buffer {
+        public:
+            VertexBuffer(void);
+            VertexBuffer(const size_t);
+            VertexBuffer(const std::vector<float> &);
+            VertexBuffer(const float [], const size_t);
+            VertexBuffer(const VertexBuffer &);
+            ~VertexBuffer(void);
+
+            const VertexBuffer &operator = (const VertexBuffer &);
+    };
+
+    class IndexBuffer : public Buffer {
+        public:
+            IndexBuffer(void);
+            IndexBuffer(const size_t);
+            IndexBuffer(const std::vector<float> &);
+            IndexBuffer(const float [], const size_t);
+            IndexBuffer(const IndexBuffer &);
+            ~IndexBuffer(void);
+
+            const IndexBuffer &operator = (const IndexBuffer &);
+    };
+
+    class Shader {
+        private:
+            GLuint m_id;
+
+        public:
+            Shader(void);
+            Shader(const GLchar *, const GLchar *);
+            Shader(const Shader &);
+            ~Shader(void);
+
+            const Shader &operator = (const Shader &);
+
+            Shader &setVertexShader(const GLchar *);        /* type: raw */
+            Shader &setVertexShader(const std::string &);   /* type: file */
+
+            Shader &setFragmentShader(const GLchar *);      /* type: raw */
+            Shader &setFragmentShader(const std::string &); /* type: file */
+
+            GLuint getID();
+    };
+
+    class Texture {
+        private:
+            GLuint m_id;
+            GLsizei m_width;
+            GLsizei m_height;
+            GLsizei m_channels;
+
+        public:
+            Texture(void);
+            Texture(const void *);          /* type: raw */
+            Texture(const std::string &);   /* type: file */
+            Texture(const Texture &);
+            ~Texture(void);
+
+            const Texture &operator = (const Texture &);
+
+            GLuint getID(void);
+            GLsizei getWidth(void);
+            GLsizei getHeight(void);
+            GLsizei getChannels(void);
+    };
+
+    class Renderer {
+        private:
+            ft::Window &m_window;
+
+            ft::Shader m_shaderDefault,
+                      *m_shaderCurrent;
+
+            ft::Texture m_textureDefault,
+                       *m_textureCurrent;
+
+            ft::VertexBuffer *m_vertexBuffer;
+            ft::IndexBuffer  *m_indexBuffer;
+
+        public:
+            Renderer(const ft::Window &);
+            Renderer(const Renderer &);
+            ~Renderer(void);
+
+            const Renderer &operator = (const Renderer &);
+
+            ft::Shader &getCurrentShader(void) const;
+            ft::Shader &getDefaultShader(void) const;
+            Renderer &setCurrentShader(const ft::Shader &);
+
+            ft::Texture &getCurrentTexture(void) const;
+            ft::Texture &getDefaultTexture(void) const;
+            Renderer &setCurrentTexture(const ft::Texture &);
+
+            ft
+    };
 };
 
 #endif /* _ft_vox_hpp_ */
